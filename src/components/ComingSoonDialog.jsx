568cba8f2a4b8dbf581a210cc58f44d3c0725022
Tran Nam <namtran09022004@gmail.com>
2024-09-17 20:37:14 +0700

    adjust ui font

--- src/components/ComingSoonDialog.jsx
+++ src/components/ComingSoonDialog.jsx
@@ -9,7 +9,7 @@ const ComingSoonDialog = ({ isOpen, onClose }) => {
             <div className="fixed inset-0 bg-black opacity-50"></div>
             <div className="relative bg-[#F9EDD2] rounded-lg shadow-lg p-6 w-full max-w-md mx-auto">
                 <div className="flex justify-between items-center border-b-2 border-[#991B1E] pb-2">
-                    <h2 className="text-2xl font-bold text-[#991B1E]">Coming Soon</h2>
+                    <h1 className="text-2xl text-[#991B1E]">Coming Soon</h1>
                     <button
                         className="text-[#991B1E] hover:text-[#F9EDD2] hover:bg-[#991B1E] rounded-full p-2 transition w-10 "
                         onClick={onClose}
